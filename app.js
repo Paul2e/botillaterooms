9412761db3dca25c72ec723472f73c928711cd32
Gwynant Jones <gwynantjones@gmail.com>
2015-02-04 14:12:20 +0000

    Make logging easier

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var startingChipCount;
 var handLimit;
 var hand;
 
+var opponents = [];
+var currentOpponent;
 var opponentMove;
 var opponentCard;
 var button;
@@ -33,12 +35,23 @@ app.use(bodyParser.urlencoded({
 }));
 app.post('/start', function(req, res) {
 
+  if (opponentName) {
+    var opponent = {
+      "name": opponentName,
+      "lastCard": opponentCard,
+      "lastMove": opponentMove
+    };
+
+    opponents.push(opponent);
+
+    console.log('Last opponent stats: ', opponent);
+  }
   opponentName = req.body.OPPONENT_NAME;
   startingChipCount = parseInt(req.body.STARTING_CHIP_COUNT);
   handLimit = req.body.HAND_LIMIT;
   hand = 1;
 
-  console.log('\n\n\n\nNew Opponent ' + opponentName + ' starting with ' + startingChipCount + ' chips for ' + handLimit + ' hands.\n\n');
+  // console.log('\n\n\n\nNew Opponent ' + opponentName + ' starting with ' + startingChipCount + ' chips for ' + handLimit + ' hands.\n\n');
 
   res.sendStatus(200);
 });
@@ -47,35 +60,34 @@ app.post('/update', function(req, res) {
 
   if (req.body.COMMAND === "CARD") {
     myCard = req.body.DATA;
-    console.log("\n\nHand: " + hand);
-  	console.log("Our card: " + myCard);
+    // console.log("\n\nHand: " + hand);
+    // console.log("Our card: " + myCard);
   }
 
   if (req.body.COMMAND === "OPPONENT_MOVE") {
     opponentMove = req.body.DATA;
-	console.log("OpponentMove: " + opponentMove);
+    // console.log("OpponentMove: " + opponentMove);
   }
 
-
   if (req.body.COMMAND === "OPPONENT_CARD") {
-  	opponentCard = req.body.DATA;
-    console.log("Opponent card: " + opponentCard);
-    hand ++;
+    opponentCard = req.body.DATA;
+    // console.log("Opponent card: " + opponentCard);
+    hand++;
   }
 
   if (req.body.COMMAND === "RECEIVE_BUTTON") {
-  	button = true;
-    console.log("We have button");
+    button = true;
+    // console.log("We have button");
   }
 
   if (req.body.COMMAND === "POST_BLIND") {
     startingChipCount -= 1;
-    console.log("POST BLIND Chip count: " + startingChipCount);
+    // console.log("POST BLIND Chip count: " + startingChipCount);
   }
 
   if (req.body.COMMAND === "RECEIVE_CHIPS") {
     startingChipCount += parseInt(req.body.DATA);
-    console.log("Received chips: " + req.body.DATA + " New count:" + startingChipCount);
+    // console.log("Received chips: " + req.body.DATA + " New count:" + startingChipCount);
   }
 
   res.sendStatus(200);
@@ -91,17 +103,16 @@ app.get('/move', function(req, res) {
   var move = cardDictionary[myCard];
 
   switch (true) {
-  	case /{amount}/.test(move):
-  		res.send("BET:10");
-  		break;
-  	case /ALL-IN/.test(move):
-  		res.send("BET:" + startingChipCount);
-  		break;
-  	default:
-  		res.send(move);
-  		break;
+    case /{amount}/.test(move):
+      res.send("BET:10");
+      break;
+    case /ALL-IN/.test(move):
+      res.send("BET:" + startingChipCount);
+      break;
+    default:
+      res.send(move);
+      break;
   }
-
 });
 
 var server = app.listen(3000, function() {
@@ -110,7 +121,6 @@ var server = app.listen(3000, function() {
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-
 });
 
 exports.app = app;
