3db1fc2e5831d57e353e83fb77113f80a630b976
Gwynant Jones <gwynantjones@gmail.com>
2015-02-04 12:13:41 +0000

    Added a card dictionary

--- app.js
+++ app.js
@@ -3,113 +3,99 @@ var app = express();
 var bodyParser = require('body-parser');
 
 var opponentName;
+var myCard;
 var startingChipCount;
 var handLimit;
-var shouldBet;
-var shouldBetAllIn;
-var shouldFold;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.post('/start', function (req, res) {
+var cardDictionary = {
+  "2": "FOLD",
+  "3": "FOLD",
+  "4": "FOLD",
+  "5": "CALL",
+  "6": "CALL",
+  "7": "CALL",
+  "8": "CALL",
+  "9": "BET",
+  "T": "BET",
+  "J": "BET",
+  "Q": "BET"
+};
 
-	shouldBet = false;
-	shouldBetAllIn = false;
-	shouldFold = false;
-	opponentName = req.body.OPPONENT_NAME;
-	startingChipCount = parseInt(req.body.STARTING_CHIP_COUNT);
-	handLimit = req.body.HAND_LIMIT;
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.post('/start', function(req, res) {
 
-	console.log('New Opponent ' + opponentName  +' starting with ' + startingChipCount + ' chips for ' + handLimit + ' hands');
+  opponentName = req.body.OPPONENT_NAME;
+  startingChipCount = parseInt(req.body.STARTING_CHIP_COUNT);
+  handLimit = req.body.HAND_LIMIT;
 
-	res.sendStatus(200);
+  console.log('New Opponent ' + opponentName + ' starting with ' + startingChipCount + ' chips for ' + handLimit + ' hands');
 
+  res.sendStatus(200);
 });
 
 app.post('/update', function(req, res) {
+  var opponentMove;
 
-	var card;
-	var opponentMove;
+  if (req.body.COMMAND === "CARD") {
+    myCard = req.body.DATA;
+  }
 
-	if (req.body.COMMAND === "CARD") {
-		card = req.body.DATA;
-	}
+  if (req.body.COMMAND === "OPPONENT_MOVE") {
+    opponentMove = req.body.DATA;
+  }
 
-	if (req.body.COMMAND === "OPPONENT_MOVE") {
-		opponentMove = req.body.DATA;
-	}
+  console.log("Our card: " + myCard);
+  console.log("OpponentMove: " + opponentMove);
 
-	if (card === "9" || card === "T" || card === "J" || card === "Q") {
-		shouldBet = true
-	} else if (card === "2" || card === "3" || card === "4") {
-		shouldFold = true
-	} else if (card === "K" || card === "A") {
-		shouldBetAllIn = true
-	}
+  if (req.body.COMMAND === "OPPONENT_CARD") {
+    console.log("Opponent card: " + req.body.DATA);
+  }
 
-	console.log("Our card: " + card);
-	console.log("OpponentMove: " + opponentMove);
+  if (req.body.COMMAND === "RECEIVE_BUTTON") {
 
-	if (req.body.COMMAND === "OPPONENT_CARD") {
+    console.log("We have button");
+  }
 
-		console.log("Opponent card: " + req.body.DATA)
-	}
+  if (req.body.COMMAND === "POST_BLIND") {
+    startingChipCount -= 1;
+    console.log("POST BLIND Chip count: " + startingChipCount);
+  }
 
-	if (req.body.COMMAND === "RECEIVE_BUTTON") {
+  if (req.body.COMMAND === "RECEIVE_CHIPS") {
+    startingChipCount += parseInt(req.body.DATA);
+    console.log("Received chips: " + req.body.DATA + " New count:" + startingChipCount);
+  }
 
-		console.log("We have button")
-	}
-
-	if (req.body.COMMAND === "POST_BLIND") {
-		startingChipCount -= 1
-		console.log("POST BLIND Chip count: " + startingChipCount)
-	}
-
-	if (req.body.COMMAND === "RECEIVE_CHIPS") {
-		startingChipCount += parseInt(req.body.DATA)
-		console.log("Received chips: " + req.body.DATA + " New count:" + startingChipCount)
-	}
-
-	res.sendStatus(200);
+  res.sendStatus(200);
 });
 
 app.get('/move', function(req, res) {
 
-	 if (startingChipCount <= 5) {
+  if (startingChipCount <= 5) {
 
-	 	console.log("Running Low. Calling");
-	 	res.send("CALL");
+    console.log("Running Low. Calling");
+    return res.send("CALL");
+  }
 
-	 } else if (shouldBet) {
+  var move = cardDictionary[myCard];
 
-		console.log("Betting");
-		res.send("BET");
+  if (move) {
+    res.send(move);
+  } else {
+    res.send("BET:" + startingChipCount);
+  }
 
-	} else if (shouldBetAllIn) {
+});
 
-		console.log("Sending: BET ALL IN:" + startingChipCount);
-		res.send("BET:" + startingChipCount);
+var server = app.listen(3000, function() {
 
-	} else if (shouldFold) {
+  var host = server.address().address;
+  var port = server.address().port;
 
-		console.log("Sending: Folding");
-		res.send("FOLD");
-	}
-	else {
+  console.log('Example app listening at http://%s:%s', host, port);
 
-		console.log("Sending: Calling");
-		res.send("CALL");
-
-	}
-	
-})
-
-var server = app.listen(3000, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
-})
+});
 
 exports.app = app;
